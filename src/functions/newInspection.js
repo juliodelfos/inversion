7f22de83894cdfdb275038e9a64cc8ad71ca26d4
Yerson <yersonolivares@protonmail.com>
2024-04-07 19:37:48 -0400

    Inserta supervisión (sin archivos aún)

--- src/functions/newInspection.js
+++ src/functions/newInspection.js
@@ -1,86 +1,54 @@
 import { supabase } from "../supabase";
 
-const newInspection = async (
-  presencia,
-  date,
-  time,
-  firma,
-  horarios_firma,
-  funcion_contrato,
-  utiliza_epp,
-  supervision_ejecutora,
-  observaciones,
-  fiscalizador,
-  RutSDV,
-  comentario_funcion_contrato,
-  comentario_utiliza_epp,
-  file,
-  herramientas,
-  condiciones_espacio_laboral,
-  comentario_condiciones_espacio_laboral,
-  comentario_condiciones_espacio_laboral,
-  comentario_utiliza_epp,
-  comentario_herramientas,
-  condiciones_maquinas,
-  comentario_condiciones_maquinas,
-  comentario_condiciones_maquinas,
-  logo_proempleo,
-  errorDatos,
-  comuna,
-  region,
-  mes,
-  nombres,
-  apellidos,
-  ejecutor,
-) => {
+const newInspection = async (info) => {
   try {
-    const { data, error } = await supabase.from("fiscalizacion").insert([
-      {
-        presencia: presencia,
-        date: date,
-        time: time,
-        firma: firma,
-        horarios_firma: horarios_firma,
-        funcion_contrato: funcion_contrato,
-        utiliza_epp: utiliza_epp,
-        supervision_ejecutora: supervision_ejecutora,
-        observaciones: observaciones,
-        fiscalizador: fiscalizador,
-        RutSDV: RutSDV,
-        comentario_funcion_contrato: comentario_funcion_contrato,
-        comentario_utiliza_epp: comentario_utiliza_epp,
-        file: acumuladorURLs.value.length > 0 ? acumuladorURLs.value : null,
-        herramientas: herramientas,
-        condiciones_espacio_laboral: condiciones_espacio_laboral,
-        comentario_condiciones_espacio_laboral:
-          comentario_condiciones_espacio_laboral,
-        comentario_utiliza_epp: comentario_utiliza_epp,
-        comentario_herramientas: comentario_herramientas,
-        condiciones_maquinas: condiciones_maquinas,
-        comentario_condiciones_maquinas: comentario_condiciones_maquinas,
-        logo_proempleo: logo_proempleo,
-        errorDatos: errorDatos,
-        comuna: comuna,
-        region: region,
-        firmaImg: imagenFirma.value,
-        mes:
-          date.split("-")[1].trim()[1] > 9
-            ? +date.split("-")[1].trim()[1]
-            : +date.split("-")[1].trim(),
-        nombres: nombres,
-        apellidos: apellidos,
-        ejecutor: ejecutor,
-      },
-    ]);
+    const { data, error } = await supabase.from("fiscalizacion").insert({
+      presencia: info.presencia,
+      // timestamp -> autogenerado
+      // id -> autogenerado
+      date: info.datetime, // fecha y hora
+      // time: info.time, -> ya no aplicaría
+      firma: info.firma,
+      horarios_firma: info.horarios_firma,
+      utiliza_epp: info.utiliza_epp,
+      supervision_ejecutora: info.supervision_ejecutora,
+      observaciones: info.observaciones,
+      funcion_contrato: info.funcion_contrato,
+      fiscalizador: info.fiscalizador,
+      RutSDV: info.RutSDV,
+      /* file:
+        info.acumuladorURLs.value.length > 0 ? acumuladorURLs.value : info.null, */ // por mientras vacío
+      //   comentario_funcion_contrato: info.comentario_funcion_contrato, -> no por ahora
+      comentario_utiliza_epp: info.comentario_utiliza_epp,
+      errorDatos: info.errorDatos,
+      herramientas: info.herramientas,
+      //   condiciones_espacio_laboral: info.condiciones_espacio_laboral, -> no por ahora
+      comentario_condiciones_espacio_laboral:
+        info.comentario_condiciones_espacio_laboral,
+      condiciones_maquinas: info.condiciones_maquinas,
+      //   comentario_condiciones_maquinas: info.comentario_condiciones_maquinas, -> no por ahora
+      logo_proempleo: info.logo_proempleo,
+      comuna: info.comuna,
+      region: info.region,
+      // firmaImg: info.imagenFirma.value, -> ya no aplicaría
+      mes:
+        info.datetime.split("-")[1],
+      nombres: info.nombres,
+      apellidos: info.apellidos,
+      ejecutor: info.ejecutor,
+      comentario_herramientas: info.comentario_herramientas,
+    });
+
+    console.log(`Supervisión de ${info.RutSDV} insertado exitosamente`);
 
     if (error) {
-      console.error("Error insertando fiscalización: ", error.message);
+      console.error("Error insertando supervisión: ", error.message);
       return;
     }
 
     return data;
   } catch (error) {
-    console.error("Error inserting new row: ", error);
+    console.error("Error insertando supervisión");
     return;
   }
 };
