b7d37b72ad26168497bd82dd85a5c32d0fb0a694
Yerson <yersonolivares@protonmail.com>
2024-04-07 14:26:03 -0400

    Se añaden plugins pro a formkit config

--- src/formkit.config.js
+++ src/formkit.config.js
@@ -1,10 +1,23 @@
 import { es } from "@formkit/i18n";
 import { defaultConfig } from "@formkit/vue";
 import { rootClasses } from "../formkit.theme.mjs";
-import { createProPlugin, inputs } from "@formkit/pro";
+import {
+  createProPlugin,
+  inputs,
+  toggle,
+  rating,
+  autocomplete,
+  mask,
+} from "@formkit/pro";
 
 const formkit_pro = import.meta.env.VITE_FORMKIT_PRO;
-const pro = createProPlugin(formkit_pro, inputs);
+const pro = createProPlugin(formkit_pro, {
+  inputs,
+  toggle,
+  rating,
+  autocomplete,
+  mask,
+});
 
 export default defaultConfig({
   locales: { es },
