2f5c890e5682569d0661eda9ccee955973fd34ff
Yerson <yersonolivares@protonmail.com>
2024-04-07 14:25:01 -0400

    Se crea vista para encuesta

---
+++ src/views/Survey.vue
@@ -0,0 +1,16 @@
+<template>
+    <!--
+  FormKit forms auto-collect values of children inputs.
+  Wrap the example in a form so we can display the form values.
+-->
+    <FormKit type="form" #default="{ value }" :actions="false">
+        <FormKit type="rating" step="0.5" name="ratingLaCasa">
+            <template #label="context">
+                <label :class="context.classes.label"
+                    >Rate your stay at <em>La Casa Madrigal</em></label
+                >
+            </template>
+        </FormKit>
+        <pre wrap>{{ value }}</pre>
+    </FormKit>
+</template>
