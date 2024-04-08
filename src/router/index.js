b559cb5b3f1d9afcaf4b45e99f2ab0b6c632e54c
Yerson <yersonolivares@protonmail.com>
2024-04-07 22:37:42 -0400

    se añade necesidad de auth para vistas

--- src/router/index.js
+++ src/router/index.js
@@ -19,28 +19,43 @@ const router = createRouter({
       path: "/crear-usuario",
       name: "crear-usuario",
       component: () => import("../views/CreateUser.vue"),
+      meta: {
+        needsAuth: true,
+      },
     },
     // Core routes
     {
       path: "/buscador",
       name: "buscador",
       component: () => import("../views/SearchWorker.vue"),
+      meta: {
+        needsAuth: true,
+      },
     },
     {
       path: "/fiscalizacion/:region/:nombres/:paterno/:materno/:ejecutor/:rut",
       name: "fiscalizacion",
       component: () => import("../views/Inspection.vue"),
+      meta: {
+        needsAuth: true,
+      },
     },
     {
       path: "/descargar-reporte",
       name: "descargar-reporte",
       component: () => import("../views/DownloadReport.vue"),
+      meta: {
+        needsAuth: true,
+      },
     },
     // Survey routes
     {
       path: "/encuesta/:region/:nombres/:paterno/:materno/:ejecutor/:rut",
       name: "encuesta",
       component: () => import("../views/Survey.vue"),
+      meta: {
+        needsAuth: true,
+      },
     },
   ],
 });
