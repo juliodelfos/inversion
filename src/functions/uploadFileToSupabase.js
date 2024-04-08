cdb7f7e2d423d8ae1cb687f99182f7b5a249c03e
Yerson <yersonolivares@protonmail.com>
2024-04-07 21:57:21 -0400

    Función para cargar imágenes a supabase

--- src/functions/uploadFileToSupabase.js
+++ src/functions/uploadFileToSupabase.js
@@ -1,17 +1,23 @@
 import { supabase } from "../supabase";
 
-const uploadFileToSupabase = async (file, pathFile) => {
-  const { photos } = file;
-  const { data, error } = await supabase.storage
-    .from("InspectionsPhotos")
-    .upload(pathFile, photos[0].file);
+const uploadFileToSupabase = async (pathFile, file) => {
+  try {
+    // console.log(`file: ${file[0].file}, pathFile: ${pathFile}`);
+    const { data, error } = await supabase.storage
+      .from("fiscalizaciones")
+      .upload(pathFile, file[0].file);
 
-  if (error) {
-    console.error("Error uploading file: ", error.statusCode);
+    if (error) {
+      console.error("Error uploading file: ", error.message);
+      return;
+    }
+
+    console.log("File uploaded: ", data);
+    return data;
+  } catch (error) {
+    console.error("Error uploading file: ", error.message);
     return;
   }
-  return data;
-  // console.log("File uploaded: ", data);
 };
 
 export { uploadFileToSupabase };
