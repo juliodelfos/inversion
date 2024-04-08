dd322b6e4f79afa0a90484876db58a5d13995d2c
yerigagarin <yersonolivares@protonmail.com>
2024-04-08 10:24:55 -0400

    Se mejora estilo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-gray-100 dark:bg-gray-900">
+    <section class="bg-blue-100 text-[#003D80] dark:bg-gray-900">
         <div
             class="flex flex-col items-center justify-center mx-auto md:h-screen lg:py-0"
         >
@@ -18,7 +18,7 @@
             >
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1
-                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                        class="text-xl font-bold leading-tight tracking-tight text-[#003D80] md:text-2xl dark:text-white"
                     >
                         Ingresar
                     </h1>
@@ -26,14 +26,14 @@
                         <div>
                             <label
                                 for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                class="block mb-2 text-sm font-medium text-[#003D80] dark:text-white"
                                 >Tu correo electrónico</label
                             >
                             <input
                                 type="email"
                                 name="email"
                                 id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-[#003D80] sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="nombre@ejemplo.com"
                                 required
                                 v-model="email"
@@ -43,7 +43,7 @@
                         <div>
                             <label
                                 for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                class="block mb-2 text-sm font-medium text-[#003D80] dark:text-white"
                                 >Contraseña</label
                             >
                             <input
@@ -52,7 +52,7 @@
                                 name="password"
                                 id="password"
                                 placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-[#003D80] sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required
                             />
                         </div>
