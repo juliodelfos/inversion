569da22b8a1fcbb305cb3bd001d8f4d13673d2f9
yerigagarin <yersonolivares@protonmail.com>
2024-04-10 09:42:55 -0400

    Corrección de estilos

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
     <main
-        class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8 border-red-300 border-2"
+        class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8"
     >
         <div class="text-center">
             <p class="text-base font-semibold">404</p>
