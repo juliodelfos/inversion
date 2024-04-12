68dc442f60978679f689dfaf74f6cfe55b21aa8e
yerigagarin <yersonolivares@protonmail.com>
2024-04-12 14:09:12 -0400

    Corrección de propiedades

--- src/utils/formkitSchema.js
+++ src/utils/formkitSchema.js
@@ -1,6 +1,7 @@
-import { comunas } from "../utils/comunasChile";
 import { formateDate } from "../functions/formateDate";
-let reportaError = false;
+
+const showErrorTextarea = window.location.search.split("=")[1];
+console.log(showErrorTextarea)
 
 const schema = [
   // {
@@ -8,7 +9,7 @@ const schema = [
   //   name: "date",
   //   label: "Fecha supervisión",
   //   help: "Fecha en que se visitó a trabajador/a",
-  //   labelClass: "labels",
+  //   "label-class": "!text-[#003D80]",
   //   classes: {
   //     help: "textoAzul",
   //   },
@@ -20,10 +21,8 @@ const schema = [
     label: "Fecha y hora de supervisión",
     help: "Fecha y hora en que se visitó a trabajador/a",
     value: formateDate(new Date().toISOString()),
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
     validation: "required|date_after:2023-01-01",
   },
   // {
@@ -31,7 +30,7 @@ const schema = [
   //   name: "time",
   //   label: "Hora supervisión",
   //   help: "Hora en que se visitó a trabajador/a",
-  //   labelClass: "labels",
+  //   "label-class": "!text-[#003D80]",
   //   classes: {
   //     help: "textoAzul",
   //   },
@@ -45,160 +44,132 @@ const schema = [
   //   label: "Comuna en que trabaja",
   //   placeholder: "Escribe la comuna",
   //   options: comunas,
-  //   labelClass: "labels",
-  //   classes: {
-  //     help: "textoAzul",
-  //   },
+  //   "label-class": "!text-[#003D80]",
+
   // },
   {
     $formkit: "toggle",
     name: "presencia",
     id: "presencia",
     label: "Se encontraba en el lugar",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "firma",
     id: "firma",
     label: "Firma libro de asistencia",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "horarios_firma",
     id: "horarios_firma",
     label: "Cumple horarios según libro",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "funcion_contrato",
     id: "funcion_contrato",
     label: "Cumple funciones según contrato",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   // {
   //   $formkit: "textarea",
   //   if: "$get(funcion_contrato).value",
   //   name: "comentario_funcion_contrato",
   //   label: "¿Qué función está realizando la trabajadora?",
-  //   labelClass: "labels",
-  //   classes: {
-  //     help: "textoAzul",
-  //   },
+  //   "label-class": "!text-[#003D80]",
   // },
   {
     $formkit: "toggle",
     name: "utiliza_epp",
     id: "utiliza_epp",
     label: "Utiliza EPP",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "textarea",
     // if: "$get(utiliza_epp).value",
     name: "comentario_utiliza_epp",
-    label: "¿Por qué motivo no utiliza sus EPP?",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    label: "Comentarios respecto a los EPP (opcional)",
+    placeholder:
+      "Mencionar motivos por el cual beneficiaria/o no utiliza elementos de protección personal.",
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "herramientas",
     id: "herramientas",
     label: "Cuenta con insumos adecuados",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "textarea",
     // if: "$get(herramientas).value",
     name: "comentario_herramientas",
-    label: "¿De qué insumo carece?",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    label: "Comentario respecto a insumos (opcional)",
+    placeholder:
+      "Mencionar motivos por el cual beneficiaria/o no ha recibido insumos.",
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "condiciones_espacio_laboral",
     id: "condiciones_espacio_laboral",
     label: "Condiciones laborales adecuadas",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   // {
   //   $formkit: "textarea",
   //   if: "$get(condiciones_espacio_laboral).value",
   //   name: "comentario_condiciones_espacio_laboral",
   //   label: "Describe falencias espacio laboral",
-  //   labelClass: "labels",
-  //   classes: {
-  //     help: "textoAzul",
-  //   },
+  //   "label-class": "!text-[#003D80]",
+  // "help-class": "!text-[#003D80]",
   // },
   // {
   //   $formkit: "toggle",
   //   name: "condiciones_maquinas",
   //   id: "condiciones_maquinas",
   //   label: "Herramientas en buen estado",
-  //   labelClass: "labels",
-  //   classes: {
-  //     help: "textoAzul",
-  //   },
+  //   "label-class": "!text-[#003D80]",
+  // "help-class": "!text-[#003D80]",
   // },
   // {
   //   $formkit: "textarea",
   //   if: "$get(condiciones_maquinas).value",
   //   name: "comentario_condiciones_maquinas",
   //   label: "Describe condiciones de máquinas o insumos",
-  //   labelClass: "labels",
-  //   classes: {
-  //     help: "textoAzul",
-  //   },
+  //   "label-class": "!text-[#003D80]",
+  // "help-class": "!text-[#003D80]",
   // },
   {
     $formkit: "toggle",
     name: "logo_proempleo",
     id: "logo_proempleo",
     label: "Indumentaria cuenta con logo ProEmpleo",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "toggle",
     name: "fiscalizacion_ejecutora",
     id: "fiscalizacion_ejecutora",
     label: "¿Ejecutora ha supervisado?",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "date",
@@ -206,39 +177,36 @@ const schema = [
     if: "$get(fiscalizacion_ejecutora).value",
     label: "Fecha última supervisión ejecutora",
     help: "Fecha en que ejecutora visitó a trabajador/a",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
   },
   {
     $formkit: "textarea",
     name: "observaciones",
     label: "Observaciones de fiscalización en terreno",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
+    placeholder: "Observaciones generales de supervisión.",
   },
   {
     $formkit: "textarea",
-    if: reportaError,
+    // if: `false`,
+    if: `${showErrorTextarea}`,
     name: "errorDatos",
     label: "Reportar errores en datos personales",
-    labelClass: "labels",
-    classes: {
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
+    placeholder: "En caso de exitir.",
   },
   {
     $formkit: "file",
     name: "file",
     label: "Cargar archivos",
-    labelClass: "labels",
-    classes: {
-      noFilesIcon: "textoAzul",
-      help: "textoAzul",
-    },
+    "label-class": "!text-[#003D80]",
+    "help-class": "!text-[#003D80]",
+    "noFilesIcon-class": "!text-[#003D80]",
+    "noFiles-class": "!text-[#003D80]",
+    "fileName-class": "!text-[#003D80]",
     accept: ".jpg, .jpeg, .png, .gif, .heif, .hevc, .docx, .xlsx",
     multiple: "false",
     // validation: "required|file",
